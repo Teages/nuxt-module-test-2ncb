a1d7290c1546d0515f349a95153d73cb6871e70a
Teages <Teages@teages.xyz>
2024-06-20 11:42:59 +0800

    commit

---
+++ playground/app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { parse } from 'graphql'
+
+const query = parse(`
+  query QueryHello($name: String) {
+    hello(name: $name)
+  }
+`)
+</script>
+
+<template>
+  <div>
+    {{ query }}
+  </div>
+</template>
